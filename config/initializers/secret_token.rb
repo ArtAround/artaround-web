4009eb545e48c0dda78cb4e7e50c7e2ca71db845
klondike <kprojection@gmail.com>
2010-09-26 17:16:18 -0400

    Regenerated to Rails 3 beta 4 for Dreamhost

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'bea328d3f6cbde576d158ccae1ca2b20139e3019cc3da29b0f7514c3d5ccdb8039bfa80fee28af94e496f3920d9436e768e40d44acbd6023e9458c3409a534e7'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Artaround::Application.routes.draw do |map|
+  root :to => "home#index"
+end
