0b69a78c1b1a8e13e1b1a5a94b0a2fc6cd794452
klondike <kprojection@gmail.com>
2010-09-27 00:22:31 -0400

    Added contact page

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,7 +1,15 @@
 class ArtsController < ApplicationController
+  before_filter :load_art
   
   def show
-    #@art = Art.first
+  end
+  
+  protected
+  
+  def load_art
+    unless params[:id] and (@art = Art.find(params[:id]))
+      head :not_found and return false
+    end
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Artaround::Application.routes.draw do |map|
   resources :arts
   
+  match "/contact" => "home#contact"
   root :to => "home#index"
 end
