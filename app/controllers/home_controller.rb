bb1e875bb1a77813c1099297a6361d391cbe5cc0
klondike <kprojection@gmail.com>
2010-09-27 23:19:42 -0400

    Enabled checkboxes, filled the lists dynamically, and made the info window behavior more standard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   
   def index
     @arts = Art.approved.all
+    @categories = Art.all.distinct :category
+    @neighborhoods = Art.all.distinct :neighborhood
   end
   
 end
