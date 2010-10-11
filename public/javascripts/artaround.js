58cbe5ea939bae0cf1fac3e146cec82fb61ff423
klondike <kprojection@gmail.com>
2010-10-11 02:57:20 -0400

    Attempt at calculating fitted sizes for the footer, will consult on how to best do it

--- public/javascripts/artaround.js
+++ public/javascripts/artaround.js
@@ -1,9 +1,31 @@
 function loadThumbnail(element_id, photo_id) {
   Flickr.getThumbnail(photo_id, "Small", function(thumbnail) {
     if (thumbnail) {
-      // TODO: figure out right size to avoid squashing
+      var max_width = 75;
+      var max_height = 75;
       
-      $("#" + element_id).html("<img src=\"" + thumbnail.source + "\"/>");
+      var rendered_height = thumbnail.height;
+      var rendered_width = thumbnail.width;
+      
+      if (thumbnail.width > thumbnail.height) {
+        if (thumbnail.width > max_width) {
+          var ratio = max_width / thumbnail.width;
+          rendered_width = max_width;
+          rendered_height = Math.floor(thumbnail.height * ratio);
+        }
+      } else {
+        if (thumbnail.height > max_height) {
+          var ratio = max_height / thumbnail.height;
+          rendered_height = max_height;
+          rendered_width = Math.floor(thumbnail.width * ratio);
+        }
+      }
+      
+      $("#" + element_id).html(
+        "<img src=\"" + thumbnail.source + "\" " + 
+          //"style=\"width: " + rendered_width + "px; height: " + rendered_height + "px\" " +
+          "/>"
+      );
     }
   });
 }
