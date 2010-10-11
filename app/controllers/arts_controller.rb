3f3c6190a0ad58d9990801fd4f4859398a82751c
klondike <kprojection@gmail.com>
2010-10-11 01:03:07 -0400

    Allowing users to upload photos that associate themselves with a piece of art

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,5 +1,5 @@
 class ArtsController < ApplicationController
-  before_filter :load_art, :only => [:show, :comment, :submit]
+  before_filter :load_art, :only => [:show, :comment, :submit, :add_photo]
   
   def new
     @art = Art.new
@@ -37,6 +37,34 @@ class ArtsController < ApplicationController
     else
       @comment = Comment.new
       render :show
+    end
+  end
+  
+  def add_photo
+    head 404 and return false unless params[:new_photo] and params[:new_photo].respond_to?(:path)
+    
+    # size check
+    if File.size(params[:new_photo].path) > (1024*1024 * 4)
+      redirect_to art_path(@art), :alert => "We ask that uploaded photos be less than 4MB in size. Please resize your photo so that it takes up less space and try again."
+      return false
+    end
+    
+    begin
+      uploads = Fleakr.upload params[:new_photo].path, {
+        :title => @art.title,
+        :tags => ["art-id-#{@art.id}", flickr[:metadata][:tag]],
+        :viewable_by => flickr[:metadata][:viewable_by],
+        :level => flickr[:metadata][:level],
+        :type => flickr[:metadata][:type]
+      }
+    rescue Fleakr::ApiError
+      redirect_to art_path(@art), :alert => "There was a problem uploading your photo. If you don't mind, please try it again."
+    else
+      @art.flickr_ids ||= []
+      @art.flickr_ids << uploads.first.id
+      @art.save! # should not be a controversial operation
+      
+      redirect_to art_path(@art), :notice => "Thank you for contributing your photo! It should appear in the slideshow below."
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Artaround::Application.routes.draw do |map|
   resources :arts do
     post :comment, :on => :member
     post :submit, :on => :member
+    post :add_photo, :on => :member
   end
   
   match "/contact" => "home#contact"
