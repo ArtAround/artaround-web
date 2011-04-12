d8bd5ad2ad64bad12e9118b05959d15d3b3f12c6
Andrei Maxim <andrei@andreimaxim.ro>
2011-04-13 00:17:15 +0300

    Looks like a working deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,10 @@ set :scm, :git
 set :repository, "git@github.com:ArtAround/artaround-web.git"
 
 set :deploy_to, "/home/dotthei/apps/#{application}"
-set :deploy_via, :copy
-set :copy_strategy, :export
+set :deploy_via, :remote_cache
 
 set :user, "dotthei"
+ssh_options[:forward_agent] = true
 set :use_sudo, false
 
 desc "Setup the staging server"
@@ -17,6 +17,16 @@ task :staging do
   set :app_server,  "staging.theartaround.us"
   set :db_server,   "staging.theartaround.us"
   set :web_server,  "staging.theartaround.us"
+
+
+  finalize_init
+end
+
+desc "Setup the production server"
+task :production do
+  set :app_server,  "theartaround.us"
+  set :db_server,   "theartaround.us"
+  set :web_server,  "theartaround.us"
 
   finalize_init
 end
@@ -27,11 +37,22 @@ task :finalize_init do
   role :web,  web_server
 end
 
+after "deploy:symlink", "deploy:finishing_touches"
+
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
+
   task :stop do ; end
+
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :finishing_touches, :roles => [:db] do
+    run "cp -pf #{shared_path}/config/admin.yml #{current_path}/config/admin.yml"
+    run "cp -pf #{shared_path}/config/flickr.yml #{current_path}/config/flickr.yml"
+    run "cp -pf #{shared_path}/config/mongoid.yml #{current_path}/config/mongoid.yml"
+    run "cp -pf #{shared_path}/config/pony.yml #{current_path}/config/pony.yml"
   end
 end
