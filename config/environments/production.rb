cae3bcaf2aaaef4310e8c53efc3aaba3ff1fbf97
Eric Mill <kprojection@gmail.com>
2012-01-22 21:12:26 -0500

    Log deprecation messages in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,8 @@ Artaround::Application.configure do
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
+  config.active_support.deprecation = :log
+
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
