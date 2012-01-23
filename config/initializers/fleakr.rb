505df784b018a9a8f3ecbec14d7fe3b87940d3e0
Eric Mill <kprojection@gmail.com>
2012-01-22 23:18:59 -0500

    On the off chance 'logger' is a reserved word

--- config/initializers/fleakr.rb
+++ config/initializers/fleakr.rb
@@ -4,6 +4,6 @@ Fleakr.api_key = @flickr[:account][:api_key]
 Fleakr.shared_secret = @flickr[:account][:shared_secret]
 Fleakr.auth_token = @flickr[:account][:auth_token]
 
-logger = Logger.new "#{Rails.root}/log/fleakr.log"
-logger.level = Logger::INFO
-Fleakr.logger = logger
+logs = Logger.new "#{Rails.root}/log/fleakr.log"
+logs.level = Logger::INFO
+Fleakr.logger = logs
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -4,6 +4,6 @@ ActionMailer::Base.smtp_settings = @email[:smtp]
 ActionMailer::Base.default_url_options = { :host => @email[:hostname] }
 ActionMailer::Base.default :from => @email[:from], :to => @email[:to]
 
-logger = Logger.new "#{Rails.root}/log/mailer.log"
-logger.level = Logger::INFO
-ActionMailer::Base.logger = logger
+logs = Logger.new "#{Rails.root}/log/mailer.log"
+logs.level = Logger::INFO
+ActionMailer::Base.logger = logs
