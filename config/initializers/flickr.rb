b0590e93b793e12651348dd0932f35fc8952846c
Laura Kogler <lkogler@pivotal-guest-257.sf.pivotallabs.com>
2013-04-09 19:42:24 -0700

    comment out flickr initializer

--- config/initializers/flickr.rb
+++ config/initializers/flickr.rb
@@ -1,7 +1,7 @@
-@flickr = YAML.load_file "#{Rails.root}/config/flickr.yml"
-
-# FlickRaw, used for other functions
-FlickRaw.api_key = @flickr[:account][:api_key]
-FlickRaw.shared_secret = @flickr[:account][:shared_secret]
-flickr.access_token = @flickr[:account][:access_token]
-flickr.access_secret = @flickr[:account][:access_secret]
+#@flickr = YAML.load_file "#{Rails.root}/config/flickr.yml"
+#
+## FlickRaw, used for other functions
+#FlickRaw.api_key = @flickr[:account][:api_key]
+#FlickRaw.shared_secret = @flickr[:account][:shared_secret]
+#flickr.access_token = @flickr[:account][:access_token]
+#flickr.access_secret = @flickr[:account][:access_secret]
