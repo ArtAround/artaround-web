356d51ea233e6172cc13f914fb10e2b1912492e1
blakeshall <blake@blakeshall.com>
2013-04-30 12:22:27 -0400

    [#47181189] Increased photo size limit to 6MB, fixed prompts and alerts to reflect new size limit

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,43 +1,43 @@
 class ArtsController < ApplicationController
   before_filter :load_art, :only => [:show, :comment, :submit, :add_photo, :flag]
-  
+
   def new
     @art = Art.new
   end
-  
+
   def create
     latitude = params[:art].delete 'latitude'
     longitude = params[:art].delete 'longitude'
-    
+
     # remove special case
     latitude = (latitude || "").strip
     longitude = (longitude || "").strip
     latitude = ["Click on Map", ""].include?(latitude) ? nil : latitude.to_f
     longitude = ["Click on Map", ""].include?(longitude) ? nil : longitude.to_f
-    
+
     @art = Art.new params[:art]
-    
+
     @art.location = [latitude, longitude] if latitude and longitude
-    
+
     @art.approved = true # auto-approve, would default to false otherwise
-    
+
     unless params[:new_photo] and params[:new_photo].respond_to?(:path)
       flash.now[:alert] = "Please include a photo of this piece of art."
       @art.valid? # generate errors
       render :new
       return false
     end
-    
+
     if @art.safely.save
       # size check
-      if File.size(params[:new_photo].path) > (1024*1024 * 2)
+      if File.size(params[:new_photo].path) > (1024*1024 * 6)
         @art.destroy
-        flash.now[:alert] = "We ask that uploaded photos be less than 2MB in size. Please resize your photo so that it takes up less space and try again."
+        flash.now[:alert] = "We ask that uploaded photos be less than 6MB in size. Please resize your photo so that it takes up less space and try again."
         render :new
       else
-      
+
         AdminMailer.new_art(@art).deliver
-        
+
         photo = create_photo @art, params[:new_photo], params[:photo_attribution_text], params[:photo_attribution_url]
         if photo.save
           redirect_to art_path(@art), :notice => "Thanks for contributing a new piece of art!"
@@ -51,41 +51,41 @@ class ArtsController < ApplicationController
       render :new
     end
   end
-  
+
   def submit
     @submission = @art.submissions.build params[:submission]
-    
+
     if @submission.save
       @art.submitted_at = Time.now
       @art.save!
-      
+
       AdminMailer.new_art_info(@art, @submission).deliver
-      
+
       redirect_to art_path(@art), :notice => "Thanks for helping us fill in the gaps! We moderate submissions, so your contribution should appear shortly."
     else
       @comment = Comment.new
       render :show
     end
   end
-  
+
   def add_photo
     redirect_to(art_path(@art)) and return false unless params[:new_photo] and params[:new_photo].respond_to?(:path)
-    
+
     # size check
-    if File.size(params[:new_photo].path) > (1024*1024 * 2)
-      redirect_to art_path(@art), :alert => "We ask that uploaded photos be less than 2MB in size. Please resize your photo so that it takes up less space and try again."
+    if File.size(params[:new_photo].path) > (1024*1024 * 6)
+      redirect_to art_path(@art), :alert => "We ask that uploaded photos be less than 6MB in size. Please resize your photo so that it takes up less space and try again."
       return false
     end
-    
+
     photo = create_photo @art, params[:new_photo], params[:photo_attribution_text], params[:photo_attribution_url]
     if photo.save
-      AdminMailer.new_photo(@art).deliver    
+      AdminMailer.new_photo(@art).deliver
       redirect_to art_path(@art), :notice => "Thank you for contributing your photo! It should appear in the slideshow below."
     else
       redirect_to art_path(@art), :alert => photo.errors.messages.values.flatten.join(" ")
     end
   end
-  
+
   def show
     @comment = Comment.new
     @submission = @art.new_submission
@@ -93,11 +93,11 @@ class ArtsController < ApplicationController
     @art.inc :total_visits, 1
     @other_photos = @art.photos.to_a.reject {|p| p == @art.primary_photo}
   end
-  
+
   def comment
     @comment = @art.comments.build params[:comment]
     @comment.ip_address = request.ip
-    
+
     if @comment.save
       AdminMailer.new_comment(@art, params[:comment]['name'], params[:comment]['text']).deliver
       redirect_to @art, :notice => "Your comment will be posted soon. Thanks for contributing!"
@@ -117,13 +117,13 @@ class ArtsController < ApplicationController
       head 201
     end
   end
-  
+
   protected
-  
+
   def load_art
     unless params[:id] and (@art = Art.where(:approved => true, :slug => params[:id]).first)
       head :not_found and return false
     end
   end
-  
+
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,18 +12,18 @@ class Photo
   field :primary, :type => Boolean, :default => false
   field :sizes, :type => Hash, :default => {}
 
-  has_mongoid_attached_file :image, 
+  has_mongoid_attached_file :image,
     :styles => {
-      :thumbnail => "20x20", 
+      :thumbnail => "20x20",
       :small => "112x112",
       :big => "500x375"
-    }, 
+    },
     :path => ':rails_root/public/system/photos/:attachment/:id/:style.:extension',
     :url => '/system/photos/:attachment/:id/:style.:extension'
 
   validates_attachment_presence :image, :message => "Please include a photo."
   validates_attachment_size :image, :in => 0..6.megabytes, :message => "Photo must be less than 6 megabytes."
-  validates_attachment_content_type :image, :content_type => ["image/jpeg", "image/gif", "image/jpg", "image/png"], :message => "Please include a JPG or PNG image for a photo."
+  validates_attachment_content_type :image, :content_type => ["image/jpeg", "image/gif", "image/jpg", "image/png"], :message => "Please include a JPG, GIF or PNG image for a photo."
 
   def attribution_text
     return self[:attribution_text] unless self[:attribution_text].blank?
