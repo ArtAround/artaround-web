41412ab6861ac1f1a7afd7acc191176d60d473ea
blakeshall <blake@blakeshall.com>
2013-04-30 11:50:16 -0400

    [45824375] Remove ward and neighboorhood from views, also removed ward numericality validation

--- app/models/art.rb
+++ app/models/art.rb
@@ -3,21 +3,21 @@ class Art
   include Mongoid::Timestamps
   include Mongoid::Slug
   include Mongoid::MultiParameterAttributes
-  
+
   has_many :comments
   belongs_to :event
   embeds_many :submissions
   has_many :photos, :dependent => :destroy
 
-  
+
   attr_protected :_id, :commissioned, :approved, :location, :slug
-  
+
   # required field, used for slug
   field :title
   slug :title
-  
+
   field :website
-  
+
   # fields doubled on submissions
   field :category
   field :artist
@@ -26,21 +26,21 @@ class Art
   field :ward, :default => ""
   field :location_description
   field :description
-  
+
   # Location (array of lat/long)
   field :location, :type => Array
-  
+
   # Address (optional, will geocode to lat/long eventually)
   field :address
   field :city
   field :state
   field :zip
-  
+
   # Used to orient street view
   field :header, :type => Float
   field :pitch, :type => Float
   field :zoom, :type => Float
-  
+
   # flags
   field :commissioned, :type => Boolean, :default => false
   field :approved, :type => Boolean, :default => false
@@ -48,7 +48,7 @@ class Art
 
   # when an art piece was last flagged by a user
   field :flagged_at, :type => DateTime
-  
+
   # last submitted at
   field :submitted_at, :type => DateTime
 
@@ -57,7 +57,7 @@ class Art
   field :api_visits, :type => Integer
   field :total_visits, :type => Integer # sum of the previous two, duplicated to make filtering easy
   field :ranking, :type => Integer
-  
+
   index [[:location, Mongo::GEO2D]]
   index :commissioned
   index :category
@@ -67,22 +67,21 @@ class Art
   index :total_visits
   index :web_visits
   index :api_visits
-  
+
   scope :commissioned, :where => {:commissioned => true}
   scope :uncommissioned, :where => {:commissioned => false}
   scope :approved, :where => {:approved => true}
   scope :unapproved, :where => {:approved => false}
   scope :featured, :where => {:featured => true}
-  
+
   scope :inbox, :where => {:approved => false}, :order_by => :created_at.desc
   scope :submitted, :order_by => :submitted_at.desc
 
   scope :popular, :where => {:ranking => {"$type" => 16}}, :order_by => :ranking.asc
-  
+
   validates_presence_of :title
   validates_presence_of :category
   validates_numericality_of :year, :allow_blank => true
-  validates_numericality_of :ward, :allow_blank => true
   validate :contains_location, :on => :create
   validates_uniqueness_of :slug
 
@@ -97,7 +96,7 @@ class Art
       Time.now.year
     end
   end
-  
+
   def primary_photo
     photos.desc(:primary).first
   end
@@ -110,12 +109,12 @@ class Art
     self.location ||= []
     location[0]
   end
-  
+
   def longitude
     self.location ||= []
     location[1]
   end
-  
+
   # new submissions should be blank, so that users are only entering the fields that they wish to correct
   def new_submission
     self.submissions.build(
@@ -132,13 +131,13 @@ class Art
   def popular?
     ranking.present?
   end
-  
+
   def contains_location
     if !latitude.present? and !longitude.present?
       if address.present? and state.present? and city.present?
         full_address = "#{address}, #{city}, #{state}"
         full_address += " #{zip}" if zip.present?
-        
+
         result = Geokit::Geocoders::GoogleGeocoder.geocode full_address
         if result.success?
           self.location = [result.lat, result.lng]
