585776d51fb1d63d88bc779887c83573b2aabe9a
blakeshall <blake@blakeshall.com>
2013-05-28 11:29:08 -0400

    Revert "API specs"
    This reverts commit ff2f5a77b55ecf96ff302a9147f78a6750bb33b8.

--- spec/factories/arts.rb
+++ spec/factories/arts.rb
@@ -3,6 +3,7 @@ require 'faker'
 FactoryGirl.define do
   factory :art do |f|
     f.title Faker::Company.catch_phrase
+    f.category ["Mural"]
     f.artist Faker::Name.name
     f.year (1900+Random.rand(114)).to_s
     f.location [Faker::Address.latitude, Faker::Address.longitude]
