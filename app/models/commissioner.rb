47feac6f432bc93533220b112dd79125923f2376
blakeshall <blake@blakeshall.com>
2013-06-20 12:23:39 -0400

    Commissioners now have logos

--- app/models/commissioner.rb
+++ app/models/commissioner.rb
@@ -1,10 +1,16 @@
 class Commissioner
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Paperclip
 
   has_many :arts, :autosave => true
 
   field :url, :type => String
   field :name, :type => String
-  field :logo
+  has_mongoid_attached_file :logo,
+    :styles => {
+      :big => "500x375"
+    },
+    :path => ':rails_root/public/system/logos/:attachment/:id/:style.:extension',
+    :url => '/system/logos/:attachment/:id/:style.:extension'
 end
