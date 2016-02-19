0182cddde212fb691f063da922f3f47061f7c060
farhan <farhan@samosys016.(none)>
2016-02-19 12:52:38 +0530

    added artist as array for web service

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -30,11 +30,14 @@ class Api::ApiController < ApplicationController
 
     results = arts.skip(skip).limit(limit).map do |art| 
       hash = art.as_json
+      if art.artist
+        hash[:artist] = art.artist.to_a
+      end 
       if art.commissioned_by.nil?
-      hash[:commissioned_by] = nil
-    else
-      hash[:commissioned_by] = {:name => art.commissioned_by.name}
-    end
+        hash[:commissioned_by] = nil
+      else
+        hash[:commissioned_by] = {:name => art.commissioned_by.name}
+      end
       hash = clean hash, art_fields
       hash[:event] = event_for_art(art)
       # hash[:photos] = photos_for_art(art)
