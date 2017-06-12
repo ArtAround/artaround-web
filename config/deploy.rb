0c655f35db26c44261806d5b185668b92abcab8a
Joel Martinez <joelgarciamartinez@gmail.com>
2017-06-12 14:48:41 -0500

    Upgrade capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,65 +1,53 @@
-# set 'target=production' in the command line to trigger production deploy
-set :environment, (ENV['target'] || 'staging')
+# config valid only for current version of Capistrano
+lock "3.8.1"
 
 set :user, 'dotthei'
-set :application, 'artaround'
-
-if environment == "staging"
-  set :gems_dir, "/home/#{user}/webapps/artaround_staging/gems"
-  set :deploy_to, "/home/#{user}/webapps/artaround_staging/artaround"
-else
-  set :gems_dir, "/home/#{user}/webapps/artaround/gems"
-  set :deploy_to, "/home/#{user}/webapps/artaround/artaround"
-end
-
-# both production and staging environments are on the same box
+set :application, "artaround"
+set :repo_url, "git@github.com:artaround/artaround-web.git"
 set :domain, 'theartaround.us'
-
-set :scm, :git
-set :repository, "git@github.com:ArtAround/artaround-web.git"
 set :branch, 'master'
 
 set :use_sudo, false
 
 set :deploy_via, :remote_cache
-set :runner, user
-set :admin_runner, runner
+set :runner, 'dotthei'
+set :admin_runner, 'dotthei'
 
-role :app, domain
-role :web, domain
+set :tmp_dir, '/home/dotthei/tmp'
+
+role :app, fetch(:domain)
+role :web, fetch(:domain)
 
 
-after "deploy", "deploy:cleanup"
-after "deploy:update_code", "deploy:shared_links"
-after "deploy:update_code", "deploy:bundle_install"
-after "deploy:update_code", "deploy:create_indexes"
+# after 'deploy', 'deploy:cleanup'
+after 'deploy:updated', 'deploy:shared_links'
+after 'deploy:updated', 'deploy:bundle_install'
+after 'deploy:updated', 'deploy:create_mongo_indexes'
 
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :migrate do; end
-  
-  desc "Restart the server"
-  task :restart, :roles => :app, :except => {:no_release => true} do
-    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
-  end
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-  desc "Create indexes"
-  task :create_indexes, :roles => :app, :except => {:no_release => true} do
-    run "cd #{release_path} && bundle exec rake db:mongoid:create_indexes"
-  end
-  
-  task :bundle_install, :roles => :app, :except => {:no_release => true} do
-    run "cd #{release_path} && bundle install --local --path=#{gems_dir}"
-  end
-  
-  desc "Get shared files into position"
-  task :shared_links, :roles => [:web, :app] do
-    run "ln -nfs #{shared_path}/config/admin.yml #{release_path}/config/admin.yml"
-    run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
-    run "ln -nfs #{shared_path}/config/pony.yml #{release_path}/config/pony.yml"
-    run "ln -nfs #{shared_path}/config/email.yml #{release_path}/config/email.yml"
-    run "rm #{File.join release_path, 'log'}"
-    run "ln -nfs #{shared_path}/log #{release_path}/log"
-  end
-end
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
---
+++ lib/capistrano/tasks/default.rake
@@ -0,0 +1,47 @@
+namespace :deploy do
+  task :start do; end
+  task :stop do; end
+  task :migrate do; end
+
+  desc 'Restart the server'
+  task :restart do
+    on roles(:app) do
+      execute "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+    end
+  end
+
+  desc 'Create mongo indexes'
+  task :create_mongo_indexes do
+    on roles(:app) do
+      # run "cd #{release_path} && bundle exec rake db:mongoid:create_indexes"
+      gem_home = "export GEM_HOME=#{fetch(:gems_dir)}"
+      ruby_lib = "export RUBYLIB=#{fetch(:app_directory)}/lib"
+      bin_path = "export PATH=#{fetch(:app_directory)}/bin:$PATH"
+
+      execute "#{gem_home} && #{ruby_lib} && #{bin_path} && cd #{release_path} && bundle exec rake db:mongoid:create_indexes"
+    end
+  end
+
+  desc 'Install dependencies'
+  task :bundle_install do
+    on roles(:app) do
+      gem_home = "export GEM_HOME=#{fetch(:gems_dir)}"
+      ruby_lib = "export RUBYLIB=#{fetch(:app_directory)}/lib"
+      bin_path = "export PATH=#{fetch(:app_directory)}/bin:$PATH"
+
+      execute "#{gem_home} && #{ruby_lib} && #{bin_path} && cd #{release_path} && bundle install --local --path #{fetch(:gems_dir)}"
+    end
+  end
+
+  desc 'Get shared files into position'
+  task :shared_links do
+    on roles(:web, :app) do
+      execute "ln -nfs #{shared_path}/config/admin.yml #{release_path}/config/admin.yml"
+      execute "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
+      execute "ln -nfs #{shared_path}/config/pony.yml #{release_path}/config/pony.yml"
+      execute "ln -nfs #{shared_path}/config/email.yml #{release_path}/config/email.yml"
+      # execute "rm #{File.join release_path, 'log'}"
+      execute "ln -nfs #{shared_path}/log #{release_path}/log"
+    end
+  end
+end
