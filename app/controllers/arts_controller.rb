5099fbf7789abe369e74f4410f97da617bf99e09
Joel Martinez <joelgarciamartinez@gmail.com>
2017-06-21 09:28:42 -0500

    Fix commenting on arts

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -122,7 +122,7 @@ class ArtsController < ApplicationController
   end
 
   def comment
-    @comment = @art.comments.build params[:comment]
+    @comment = @art.comments.build comment_params
     @comment.ip_address = request.ip
 
     if @comment.save
@@ -244,6 +244,10 @@ class ArtsController < ApplicationController
       :description, :location_description, :latitude, :longitude, :address,
       :city, :state, :zip, category: [], tag: []
     )
+  end
+
+  def comment_params
+    params.require(:comment).permit(:name, :email, :url, :text)
   end
 
   def load_art
